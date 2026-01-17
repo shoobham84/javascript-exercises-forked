4250f73d7eeb2dfdd0e9fe88227ac17fd2e1ea72
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-01-17 16:01:19 +0530

    Do 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    if (count < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < count; i++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
