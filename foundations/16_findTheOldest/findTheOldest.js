ea496c4c7b9c9c93b3a405573f1faef59aad0ffa
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-01-26 17:20:05 +0530

    Do 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+        return currentAge > oldestAge ? current : oldest;
+    })
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
