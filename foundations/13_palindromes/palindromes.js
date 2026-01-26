494f30bd0973da0af084b82e1a3466c777d6cfb4
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-01-26 16:46:52 +0530

    Do 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cl = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, '')
+    .split('')
+    .join('');
 
+    let left = 0, right = cl.length - 1;
+    while (left < right) {
+        if (cl[left] !== cl[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
