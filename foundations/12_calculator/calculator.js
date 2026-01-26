840a8a547ae43556a85744bf59cf5ef010d2a46b
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-01-26 16:36:19 +0530

    Do 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item);
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	return b ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let f = 1;
+	for (let i = a; i > 0; --i) {
+    f *= i;
+  }
+  return f;
 };
 
 // Do not edit below this line
